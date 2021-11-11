83e4ff85f952907c9a59839dd7a03026a3e90254
Andrew <andrew@hugheswd.com>
2021-11-11 08:00:24 -0800

    Basic prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include <Wire.h>
 #include <Adafruit_I2CDevice.h>
 #include <RotaryEncoder.h>
+#include <LiquidCrystal.h>
 
-#define NUM_LEDS 60
-#define LED_PIN 6
+#define NUM_LEDS 300
+#define LED_PIN 5
 #define COLOR_ORDER RGB
 int bgt = 20;
 CRGB leds[NUM_LEDS];
+int hue = 100;
+
+#define SOLID 0
+#define PCMR 1
+#define FALL_HOLIDAY 2
+#define XMAS 3
+#define MURICA 4
+#define PRIDE 5
+
+#define FX_TOTAL 6
+int fx = SOLID;
+String fxName = "Solid";
 
 #define ENCODER_SW 4
 #define ENCODER_DT 3
 #define ENCODER_CLK 2
 RotaryEncoder *encoder = nullptr;
 
-int hue = 100;
+const int RS = 13, EN = 12, D4 = 11, D5 = 10, D6 = 9, D7 = 8;
+LiquidCrystal lcd(RS,EN,D4,D5,D6,D7); 
 
 // -----------------------------------------------------
 
@@ -28,48 +41,143 @@ int readEncoder(int item, boolean isColor) {
       newItem = 255;
     }
   }
-  // int pos = 0;
-  // int newPos = encoder->getPosition();
-  // if (pos != newPos) {
-  //   Serial.print("pos:");
-  //   Serial.print(newPos);
-  //   Serial.print(" dir:");
-  //   Serial.println((int)(encoder->getDirection()));
-  //   pos = newPos;
-  // }
   return newItem;
 }
 
+void readEncoderBtn() {
+  if (digitalRead(ENCODER_SW) == LOW) {
+    fx++;
+    if (fx >= FX_TOTAL) {
+      fx = 0;
+    }
+    delay(200);
+  }
+  
+}
+
 void displayLights() {
-  for(int i = 0; i < NUM_LEDS; i++) {
+  switch (fx)
+  {
+  case PCMR:
+      fxName = "PCMR";
+      fill_rainbow( leds, NUM_LEDS, hue, 2);
+      hue++;
+      if (hue > 255) {
+        hue = 0;
+      }
+      
+    break;
+
+  case FALL_HOLIDAY:
+    fxName = "Fall";
+    for (size_t i = 0; i < NUM_LEDS; i+=16) {
+      for (size_t n = 0; n < 8; n++) {
+        int indexA = i+n;
+        int indexB = i+n+8;
+        if (indexA < NUM_LEDS) {
+          leds[indexA] = CRGB().setHSV(25, 255, 255);
+        }
+        if (indexB < NUM_LEDS) {
+          leds[indexB] = CRGB::White;
+        }
+      }
+      
+    }
+    
+    break;
+  
+  case XMAS:
+    fxName = "Xmas";
+    for (size_t i = 0; i < NUM_LEDS; i+=16) {
+      for (size_t n = 0; n < 8; n++) {
+        int indexA = i+n;
+        int indexB = i+n+8;
+        if (indexA < NUM_LEDS) {
+          leds[indexA] = CRGB::Red;
+        }
+        if (indexB < NUM_LEDS) {
+          leds[indexB] = CRGB::Green;
+        }
+      }
+    }
+    break;
+  
+  case MURICA:
+    fxName = "Murica!";
+    for (size_t i = 0; i < NUM_LEDS; i+=27) {
+      for (size_t n = 0; n < 9; n++) {
+        int indexA = i+n;
+        int indexB = i+n+9;
+        int indexC = i+n+9+9;
+        if (indexA < NUM_LEDS) {
+          leds[indexA] = CRGB::Red;
+        }
+        if (indexB < NUM_LEDS) {
+          leds[indexB] = CRGB::White;
+        }
+        if (indexC < NUM_LEDS) {
+          leds[indexC] = CRGB::Blue;
+        }
+      }
+    }
+    break;
+
+  case PRIDE:
+  fxName = "Pride";
+    fill_rainbow( leds, NUM_LEDS, hue, 2);
+    break;
+  
+  default:
+
+  fxName = "Solid";
+    for(int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB().setHSV(hue, 255, 255);
+    }
+    break;
   }
   FastLED.show();
+}
+
+void displayLcd() {
+  lcd.home();
+  lcd.clear();
+  lcd.print(fxName);
+  lcd.setCursor(0, 2);
+  lcd.print("hue: ");
+  lcd.print(hue);
 }
 
 // -----------------------------------------------------
 
 void checkPosition() {
-  encoder->tick(); // just call tick() to check the state.
+  encoder->tick();
 }
 
 void setup() {
-  delay(1000);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+
   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(bgt);
 
   // encoder = new RotaryEncoder(ENCODER_CLK, ENCODER_DT, RotaryEncoder::LatchMode::TWO03);
-  // encoder = new RotaryEncoder(ENCODER_CLK, ENCODER_DT, RotaryEncoder::LatchMode::FOUR3);
-  encoder = new RotaryEncoder(ENCODER_CLK, ENCODER_DT, RotaryEncoder::LatchMode::FOUR0);
+  encoder = new RotaryEncoder(ENCODER_CLK, ENCODER_DT, RotaryEncoder::LatchMode::FOUR3);
+  // encoder = new RotaryEncoder(ENCODER_CLK, ENCODER_DT, RotaryEncoder::LatchMode::FOUR0);
   attachInterrupt(digitalPinToInterrupt(ENCODER_DT), checkPosition, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), checkPosition, CHANGE);
 
-  Serial.begin(9600);
+  lcd.begin(16, 2);
+
+  // Serial.begin(9600);
 }
 
 void loop() {
   hue = readEncoder(hue, true);
-  displayLights();
+  readEncoderBtn();
+  // displayLights();
+  displayLcd();
 
-  Serial.println(hue);
+  EVERY_N_MILLISECONDS( 20 ) { displayLights(); }
+
+  // Serial.println(hue);
+  // delay(100);
 }
